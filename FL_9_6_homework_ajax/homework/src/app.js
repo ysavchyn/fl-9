de089cbab54ce43247b42dc9687bb182a435d727
Yaroslav Savchyn <34660791+ysavchyn@users.noreply.github.com>
2018-10-10 22:41:52 +0300

    Homework_6_beta2
    --> a lot of improvements

--- FL_9_6_homework_ajax/homework/src/app.js
+++ FL_9_6_homework_ajax/homework/src/app.js
@@ -1,12 +1,14 @@
 let trackButton = document.getElementById('track'),
     resetButton = document.getElementById('reset'),
-    inWater = document.getElementById('in-water'),
-    onEarth = document.getElementById('on-earth'),
+    idLocation = document.getElementById('location'),
     mainForm = document.getElementById('main-form'),
-    data = document.getElementById('data'),
+    info = document.getElementById('info'),
     inputLatitude = document.getElementById('latitude'),
     inputLongitude = document.getElementById('longitude'),
-    loading = document.getElementById('loading');
+    loading = document.getElementById('loading'),
+    h1 = document.createElement('h1');
+
+idLocation.appendChild(h1);
 
 let http = {};
 
@@ -35,7 +37,10 @@ http.get = url => {
         };
 
         request.onerror = () => {
-            reject(request.statusText);
+            reject({
+                status: request.status,
+                response: request.response
+            });
         };
 
         request.send(null);
@@ -43,43 +48,51 @@ http.get = url => {
 };
 
 trackButton.onclick = function () {
-    let latitude = inputLatitude.value,
-        longitude = inputLongitude.value;
+    let latitude = parseFloat(inputLatitude.value),
+        longitude = parseFloat(inputLongitude.value);
 
     http.get(`https://api.onwater.io/api/v1/results/${latitude},${longitude}`).then(response => {
-        //console.log(response); //for debug
         let parsedResponse = JSON.parse(response);
+
         if (parsedResponse.water) {
-            inWater.style.display = 'block';
+            idLocation.className = 'in-water blur-in';
+            idLocation.firstElementChild.innerHTML = 'In the water';
         } else {
-            onEarth.style.display = 'block';
+            idLocation.className = 'on-earth blur-in';
+            idLocation.firstElementChild.innerHTML = 'On earth';
         }
-        data.innerHTML = `<b>Latitude:</b> ${parsedResponse.lat}, <b>longitude:</b> ${parsedResponse.lon}`;
+
+        info.className = 'blur-in';
+        info.innerHTML = `Latitude: ${parsedResponse.lat}, longitude: ${parsedResponse.lon}`;
+
         resetButton.style.display = 'block';
     }).catch(e => {
         let status = e.status,
             response = e.response;
 
-        if (status === 400) {
-            data.innerHTML = response;
-        } else {
-            data.innerHTML = `Error code: ${status}`;
+        if (status === 400 || status === 429) {
+            info.className = 'blur-in';
+            info.innerHTML = JSON.parse(response).errors || JSON.parse(response).error;
         }
 
+        idLocation.className = 'error blur-in';
+        idLocation.firstElementChild.innerHTML = `E: ${status}`;
         resetButton.style.display = 'block';
     });
+
+    info.className = '';
 
     return false;
 };
 
 resetButton.onclick = () => {
-    inWater.style.display = 'none';
-    onEarth.style.display = 'none';
+    idLocation.className = '';
+    idLocation.firstElementChild.innerHTML = '';
     resetButton.style.display = 'none';
     mainForm.style.display = 'flex';
     inputLatitude.value = '';
     inputLongitude.value = '';
-    data.innerHTML = '';
+    info.innerHTML = '';
 
     return false;
 };
